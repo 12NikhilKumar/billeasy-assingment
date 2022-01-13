00bff428444e4921fd7a4ee68be7d7c5304118f8
Nikhil kumar <egkumarnikhil16@gmail.com>
2022-01-13 15:18:44 +0530

    gallery page assingment

---
+++ src/galery/galerypage.jsx
@@ -0,0 +1,44 @@
+import axios from "axios";
+import React from "react";
+import styles from "./galerypage.module.css";
+const Card = ({url}) => {
+    const [name,setName] = React.useState(styles.card)
+     const handleClick = ()=>{
+        if(name == styles.card){
+            setName(styles.card1)
+        }
+        else{
+            setName(styles.card)
+        }
+    }
+    return (
+        <div onClick={handleClick} className={name}>
+            <img src={url} alt="img"/>
+        </div>
+    )
+}
+const Galery = ()=>{
+    const [data,setData] = React.useState(null);
+    const [extend,setExtend] = React.useState(false);
+    React.useEffect(()=>{
+        const config = {
+            method: 'GET',
+            url: 'http://www.mocky.io/v2/5ecb5c353000008f00ddd5a0'
+        }
+        axios(config)
+        .then((res)=>{
+            setData(res.data)
+        })
+    },[]);
+    return (
+        <>
+        <h1>Gallery Page</h1>
+        <div className={styles.grid}>
+            {data?.map((item)=>(
+                <Card  key={item.id} url={item.urls.regular}/>
+            ))}
+        </div>
+        </>
+    )
+}
+export default Galery
